228a3f4a536c632df2b24df549029dfd5a2db0f6
Princepal <princepal.singh@thewitslab.com>
2022-03-30 18:40:08 +0530

    initial Commit

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,28 @@
+import ExpenseDate from "./ExpenseDate";
+import "./ExpenseItem.css";
+import Card from '../UI/Card'
+import { useState } from "react";
+
+const ExpenseItem=(props)=> {
+  const [title,setTitle] = useState(props.title);
+  // let title = props.title;
+  const clickHandler =()=>{
+    // title = "UPDATED";
+    // console.log(title)
+    setTitle("updated")
+  }
+ 
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+        
+      </div>
+      <button onClick={clickHandler}>Change Title</button>
+    </Card>
+  );
+}
+
+export default ExpenseItem;
